81ecc914192ab7ef01431d5718b743bfb96aad99
Logan Welch <loganewelch@gmail.com>
2023-09-01 13:30:27 -0500

    added query to show respective total sales for 2009 and 2011

---
+++ total_sales_{year}.sql
@@ -0,0 +1,10 @@
+SELECT
+    SUBSTR(invoiceDate, 1, 4) AS "Year",
+    SUM(total) AS "Total Sales"
+FROM
+    Invoice
+WHERE
+    SUBSTR(invoiceDate, 1, 4) IN ('2009', '2011')
+GROUP BY
+    "Year";
+
